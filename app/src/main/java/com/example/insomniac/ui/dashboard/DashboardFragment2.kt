51d16e3b76f6d20e0429174766652b110d7f6855
Michel Davies <micheldavies0288@gmail.com>
2021-11-07 20:00:38 -0500

    "Added Changes to Dashboard"

--- app/src/main/java/com/example/insomniac/ui/dashboard/DashboardFragment2.kt
+++ app/src/main/java/com/example/insomniac/ui/dashboard/DashboardFragment2.kt
@@ -4,6 +4,7 @@ import android.icu.util.Calendar
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -35,7 +36,10 @@ class DashboardFragment2 : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         currentDateTime(view)
+        StartTime(view)
     }
+
+    //clean up code ...probably change to activity..highlight background on circle
     private fun currentDateTime(view: View)= Thread {
         while (run) {
             try {
@@ -46,6 +50,27 @@ class DashboardFragment2 : Fragment() {
 //                    val full_date=SimpleDateFormat("EEE").format(c.getTime())+" "+SimpleDateFormat("MMMM").format(c.getTime())+" "+SimpleDateFormat("d").format(c.getTime())+","+SimpleDateFormat("yyyy").format(c.getTime())
                     val showView=view.findViewById<TextView>(R.id.text_dashboard2)
                     showView.text=fullTime.toString()
+                }
+            } catch (e: Exception) {
+            }
+        }
+    }.start()
+
+    //clean up code ...probably change to activity..highlight background on circle
+    private fun StartTime(view: View)= Thread {
+        var seconds=0
+        while (run) {
+            try {
+                Thread.sleep(1000)
+                Handler(Looper.getMainLooper()).post {
+
+                    seconds += 1
+                    var hours = seconds/3600
+                    var minutes=(seconds%3600)/60
+
+                    Log.i("test",hours.toString()+""+minutes.toString()+""+seconds.toString())
+                    val showCount=view.findViewById<TextView>(R.id.Time_Slp2)
+                    showCount.text=(hours.toString()+"h "+minutes.toString()+"m").toString()
                 }
             } catch (e: Exception) {
             }
