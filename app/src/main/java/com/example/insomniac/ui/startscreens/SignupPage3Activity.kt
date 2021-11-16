f0e9692e434d6c92187ac9bef2f37f3b825877ed
Michel Davies <micheldavies0288@gmail.com>
2021-11-16 18:58:18 -0500

    "Timer fixed"

--- app/src/main/java/com/example/insomniac/ui/startscreens/SignupPage3Activity.kt
+++ app/src/main/java/com/example/insomniac/ui/startscreens/SignupPage3Activity.kt
@@ -9,8 +9,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import com.example.insomniac.R
 import com.example.insomniac.ui.dashboard.DashboardStartActivity
-import com.example.insomniac.model.UserViewModel
-import com.example.insomniac.model.user.User
+//import com.example.insomniac.model.UserViewModel
+//import com.example.insomniac.model.user.User
 
 class SignupPage3Activity: AppCompatActivity() {
     var username: String = "";
@@ -26,24 +26,24 @@ class SignupPage3Activity: AppCompatActivity() {
     var sleepApnea: Boolean = false;
     var narcolepsy: Boolean = false;
 
-    lateinit var user:User
-    lateinit var userviewmodel: UserViewModel
+//    lateinit var user:User
+//    lateinit var userviewmodel: UserViewModel
 
 
     @SuppressLint("ClickableViewAccessibility")
     private val startTouchListener = View.OnTouchListener { view, motionEvent ->
         when (motionEvent.action) {
             MotionEvent.ACTION_DOWN -> {
-                user = User(0, this.username, this.gender, this.age, this.feet, this.inches, this.weight,
-                                        this.fitness, this.insomnia, this.sleepApnea, this.narcolepsy)
-                userviewmodel.insert(user);
+//                user = User(0, this.username, this.gender, this.age, this.feet, this.inches, this.weight,
+//                                        this.fitness, this.insomnia, this.sleepApnea, this.narcolepsy)
+//                userviewmodel.insert(user);
                 val intent = Intent(this, DashboardStartActivity::class.java).apply {}
                 startActivity(intent);
             }
             MotionEvent.ACTION_UP -> {
-                user = User(0, this.username, this.gender, this.age, this.feet, this.inches, this.weight,
-                    this.fitness, this.insomnia, this.sleepApnea, this.narcolepsy)
-                userviewmodel.insert(user);
+              //  user = User(0, this.username, this.gender, this.age, this.feet, this.inches, this.weight,
+//                    this.fitness, this.insomnia, this.sleepApnea, this.narcolepsy)
+              //  userviewmodel.insert(user);
                 val intent = Intent(this, DashboardStartActivity::class.java).apply {}
                 startActivity(intent);
             }
@@ -54,13 +54,13 @@ class SignupPage3Activity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.signup_page3)
 
-        this.username = intent.getStringExtra("USERNAME").toString()
-        this.gender = intent.getStringExtra("GENDER").toString()
-        this.age = intent.getIntExtra("AGE", 0);
-        this.feet = intent.getIntExtra("FEET", 0);
-        this.inches = intent.getIntExtra("INCHES", 0);
-        this.weight = intent.getIntExtra("WEIGHT", 0);
-        this.fitness = intent.getStringExtra("FITNESS").toString()
+//        this.username = intent.getStringExtra("USERNAME").toString()
+//        this.gender = intent.getStringExtra("GENDER").toString()
+//        this.age = intent.getIntExtra("AGE", 0);
+//        this.feet = intent.getIntExtra("FEET", 0);
+//        this.inches = intent.getIntExtra("INCHES", 0);
+//        this.weight = intent.getIntExtra("WEIGHT", 0);
+//        this.fitness = intent.getStringExtra("FITNESS").toString()
 
         findViewById<Button>(R.id.nextStep3).setOnTouchListener(startTouchListener)
     }
