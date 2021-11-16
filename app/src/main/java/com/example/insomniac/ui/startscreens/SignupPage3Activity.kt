506b7b3f3e95aa3b5ac1996be7997ed167b92688
Jantae Leckie <jleckie@altoleap.com>
2021-11-16 15:57:23 -0500

    Insomniac DB - Some DB functionality added #2

--- app/src/main/java/com/example/insomniac/ui/startscreens/SignupPage3Activity.kt
+++ app/src/main/java/com/example/insomniac/ui/startscreens/SignupPage3Activity.kt
@@ -26,24 +26,24 @@ class SignupPage3Activity: AppCompatActivity() {
     var sleepApnea: Boolean = false;
     var narcolepsy: Boolean = false;
 
-    lateinit var user:User
-    lateinit var userviewmodel: UserViewModel
+//    lateinit var user:User
+//    lateinit var userviewmodel: UserViewModel
 
 
     @SuppressLint("ClickableViewAccessibility")
     private val startTouchListener = View.OnTouchListener { view, motionEvent ->
         when (motionEvent.action) {
             MotionEvent.ACTION_DOWN -> {
-                user = User(0, this.username, this.gender, this.age, this.feet, this.inches, this.weight,
-                                        this.fitness, this.insomnia, this.sleepApnea, this.narcolepsy)
-                userviewmodel.insert(user);
+//                user = User(0, this.username, this.gender, this.age, this.feet, this.inches, this.weight,
+//                                        this.fitness, this.insomnia, this.sleepApnea, this.narcolepsy)
+//                userviewmodel.insert(user);
                 val intent = Intent(this, DashboardStartActivity::class.java).apply {}
                 startActivity(intent);
             }
             MotionEvent.ACTION_UP -> {
-                user = User(0, this.username, this.gender, this.age, this.feet, this.inches, this.weight,
-                    this.fitness, this.insomnia, this.sleepApnea, this.narcolepsy)
-                userviewmodel.insert(user);
+//                user = User(1, this.username, this.gender, this.age, this.feet, this.inches, this.weight,
+//                    this.fitness, this.insomnia, this.sleepApnea, this.narcolepsy)
+//                userviewmodel.insert(user);
                 val intent = Intent(this, DashboardStartActivity::class.java).apply {}
                 startActivity(intent);
             }
